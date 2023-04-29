64c869bc4c28b3ed44ee4845b49a00b3ab852f42
KaiSomething <kaialbertson@gmail.com>
2023-04-28 18:29:38 -0700

    added indicator to tell when program is running

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var base_url = "https://207.81.219.142:1945/run/"
 //var base_url = "https://192.168.1.200:1945/run/"
 var running = false
+var button_element = document.getElementsByClassName("run-button")[0];
+var input_element = document.getElementById("input");
+var output_element = document.getElementById("output");
 
 function run_code(){
-    input = document.getElementById("input");
-    code = input.value;
+    code = input_element.value;
     code_url = encodeURIComponent(code);
 
     if(!running){
         running = true
+        button_element.classList.add("running")
+        button_element.innerText = "Running..."
         console.log(code_url)
         
         fetch(base_url + code_url, {
@@ -16,9 +20,10 @@ function run_code(){
         }).then(function(response) {
             return response.json();
         }).then(function(data) {
-            output = document.getElementById("output");
-            output.innerText = data.out
+            output_element.innerText = data.out
             running = false
+            button_element.classList.remove("running")
+            button_element.innerText = "Run"
         }).catch(function(err) {
             base_url = "https://192.168.1.200:1945/run/"
             fetch(base_url + code_url, {
@@ -26,9 +31,10 @@ function run_code(){
             }).then(function(response) {
                 return response.json();
             }).then(function(data) {
-                output = document.getElementById("output");
-                output.innerText = data.out
+                output_element.innerText = data.out
                 running = false
+                button_element.classList.remove("running")
+                button_element.innerText = "Run"
             }).catch(function(err) {
                 alert("Servers are currently down.");
             });
